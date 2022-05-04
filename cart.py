1fb45faf94d438bdc95b90bc55d6b37c5e1bc416
DawgBoi <104690907+DawgBoi@users.noreply.github.com>
2022-05-03 21:38:51 -0500

    Update cart.py

--- cart.py
+++ cart.py
@@ -36,35 +36,35 @@ class cart:
         return self.UserID
 
     def getMovieID(self):
-        query = "SELECT MovieID FROM cart WHERE UserID=%d"
+        query = "SELECT MovieID FROM cart WHERE UserID=%s"
         data = self.UserID
         cursor.execute(query,data)
         result = cursor.fetchall()
         return result
 
     def getTitle(self):
-        query = "SELECT Title FROM cart WHERE UserID=%d"
+        query = "SELECT Title FROM cart WHERE UserID=%s"
         data = self.UserID
         cursor.execute(query,data)
         result = cursor.fetchall()
         return result
 
     def getAmount(self):
-        query = "SELECT amount FROM cart WHERE UserID=%d"
+        query = "SELECT amount FROM cart WHERE UserID=%s"
         data = self.UserID
         cursor.execute(query,data)
         result = cursor.fetchall()
         return result
 
     def getTotal(self):
-        query = "SELECT total FROM cart WHERE UserID=%d"
+        query = "SELECT total FROM cart WHERE UserID=%s"
         data = self.UserID
         cursor.execute(query,data)
         result = cursor.fetchall()
         return result
 
     def dispCart(self):
-        query = "SELECT * FROM cart WHERE UserID=%d"
+        query = "SELECT * FROM cart WHERE UserID=%s"
         data = self.UserID
         cursor.execute(query,data)
         result = cursor.fetchall()
@@ -72,10 +72,27 @@ class cart:
             print("Movie ID: ", x[1], "\nTitle: ", x[2], "\nAmount: ", x[3], "\nTotal: ", x[4], "\nDate Ordered: ", x[5])
         return
 
+    def remItem(self, Title):
+        query = "SELECT * FROM cart WHERE UserID=%s"
+        data = self.UserID
+        cursor.execute(query,data)
+        result = cursor.fetchall()
+        for x in cart:
+            if Title==x[2]:
+                toReturn = x
+                query = "DELETE FROM cart WHERE Title=%s"
+                data = Title
+                cursor.execute(query,data)
+                connection.commit()
+                return toReturn
+            else:
+                print("Item not in cart")
+                return
+        
+
     def addToCart(MovieID,Title,amount,total):
-        query = "INSERT INTO `cart` (`UserID`, `MovieID`, `Title`, `amount`, `total`) VALUES (%d,%d,%s,%d,%d)"
+        query = "INSERT INTO cart (UserID, MovieID, Title, amount, total) VALUES (%s,%s,%s,%s,%s)"
         data = (self.UserID,MovieID,Title,amount,total)
         cursor.execute(query,data)
         print("Added to cart")
-        return 
-  
+        return   

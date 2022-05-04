da85f8f447e7cb9b67d7c7a3a2eb51521bb323ce
Trey Herrington <45339364+trey-herrington@users.noreply.github.com>
2022-05-03 22:00:34 -0500

    Update user.py
    Updating to add shipping information

--- user.py
+++ user.py
@@ -30,43 +30,67 @@ class user:
         query = "SELECT Username FROM users WHERE UserID = \"%d\"" % self.userID
         cursor.execute(query)
         result = cursor.fetchall()
-        return result[0]
+        return result[0][0]
 
     def getName(self):
         query = "SELECT Name FROM users WHERE UserID = \"%d\"" % self.userID
         cursor.execute(query)
         result = cursor.fetchall()
-        return result[0]
+        return result[0][0]
 
     def getBirthday(self):
         query = "SELECT Birthday FROM users WHERE UserID = \"%d\"" % self.userID
         cursor.execute(query)
         result = cursor.fetchall()
-        return result[0]
+        return result[0][0]
 
     def getCreditCardnum(self):
         query = "SELECT CreditCardnum FROM users WHERE UserID = \"%d\"" % self.userID
         cursor.execute(query)
         result = cursor.fetchall()
-        return result[0]
+        return result[0][0]
 
     def getcvv(self):
         query = "SELECT cvv FROM users WHERE UserID = \"%d\"" % self.userID
         cursor.execute(query)
         result = cursor.fetchall()
-        return result
+        return result[0][0]
 
     def getCCexpire(self):
         query = "SELECT getCCexpire FROM users WHERE UserID = \"%d\"" % self.userID
         cursor.execute(query)
         result = cursor.fetchall()
-        return result[0]
+        return result[0][0]
 
     def getPassword(self):
         query = "SELECT Password FROM users WHERE UserID = \"%d\"" % self.userID
         cursor.execute(query)
         result = cursor.fetchall()
-        return result[0]
+        return result[0][0]
+    
+    def getAddress(self):
+        query = "SELECT Address FROM users WHERE UserID = \"%d\"" % self.userID
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result[0][0]
+    
+    def getCity(self):
+        query = "SELECT City FROM users WHERE UserID = \"%d\"" % self.userID
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result[0][0]
+    
+    def getState(self):
+        query = "SELECT State FROM users WHERE UserID = \"%d\"" % self.userID
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result[0][0]
+    
+    def getZIP(self):
+        query = "SELECT ZIP FROM users WHERE UserID = \"%d\"" % self.userID
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result[0][0]
 
     def viewUserInformation(self):
         query = "SELECT * FROM users WHERE UserID = \"%d\"" % self.userID
@@ -74,16 +98,16 @@ class user:
         result = cursor.fetchall()
         for x in result:
             print("UserID: ", x[0], "\nName: ", x[1], "\nBirthday: ", x[2], "\nCredit Card Number: ", x[3], "\nCVV: ",
-                  x[4], "\nCCexpire: ", x[5], "\nPassword: ", \
-                  x[6], "\n")
+                  x[4], "\nCCexpire: ", x[5], "\nPassword: ", x[6], "\nAddress: ", x[7], "\nCity: ", x[8], "\nState: ", x[9],
+                  "\nZIP: ", x[10], "\n")
 
-    def pushUser(self, Username, Name, Birthday, CreditCardnum, cvv, CCexpire, Password):
+    def pushUser(self, Username, Name, Birthday, CreditCardnum, cvv, CCexpire, Password, Address, City, State, ZIP):
         user_query = "SELECT MAX(userID) FROM users"
         cursor.execute(user_query)
         UserID = cursor.fetchall()
         New_UserID = str(UserID[0][0] + 1)
-        query = "INSERT INTO users (UserID, Username, Password, Name, Birthday, CreditCardnum, cvv, CCexpire) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-        data = (New_UserID, Username, Password, Name, Birthday, CreditCardnum, cvv, CCexpire)
+        query = "INSERT INTO users (UserID, Username, Password, Name, Birthday, CreditCardnum, cvv, CCexpire, Address, City, State, ZIP) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        data = (New_UserID, Username, Password, Name, Birthday, CreditCardnum, cvv, CCexpire, Address, City, State, ZIP)
         cursor.execute(query, data)
         connection.commit()
         print("User added to system\n")
